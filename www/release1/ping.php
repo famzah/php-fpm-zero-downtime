740b6e1f2b446c47a95ceec8f7cfdaa5884f2135
Ivan Zahariev <famzah@famzah.net>
2024-12-20 18:44:53 +0200

    Initial commit

---
+++ www/release1/ping.php
@@ -0,0 +1,2 @@
+<?php
+echo "pong1\n";
---
+++ www/release2/sleep-long.php
@@ -0,0 +1,13 @@
+<?php
+$targetSleepTime = 5.0; // Total desired sleep time in seconds
+
+$startTime = microtime(true); // Record the start time
+
+while ((microtime(true) - $startTime) < $targetSleepTime) {
+    usleep(100000); // Sleep for 0.1 seconds
+}
+
+$currDir = getcwd();
+
+echo "ver2: Success. Slept for $targetSleepTime seconds. CWD: $currDir\n";
+?>
